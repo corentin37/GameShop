1b653dc81103532690be6bf3a775162f79ef22d2
Damien <damien.lahaussois@gmail.com>
2021-05-14 17:43:38 +0200

    Page de connexion (front uniquement)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JeuComponent } from './jeu/jeu.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JeuComponent
+    JeuComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
