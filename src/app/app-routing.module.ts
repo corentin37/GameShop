31f197b6c0544c9802b1e9165aa8ab07f4f6e72a
Floriane <floriane.richard2@gmail.com>
2021-05-14 16:47:17 +0200

    Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JeuComponent } from './jeu/jeu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: JeuComponent, path:'jeu'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
