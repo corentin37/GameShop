3adc7ab7e11417b201e09f0c7051903d01fbfb2c
possart <alexis.cadoux@gmail.com>
2021-05-16 00:06:36 +0200

    CLC

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,23 +9,27 @@ import { Router } from '@angular/router';
 })
 export class AdminComponent implements OnInit {
 
-  user; joueur; admin; vendeur;
+  users; joueurs; admins; vendeurs; rechercheV ; recherche; entreeConsole;
   constructor(private http: HttpClient, private route: Router) { }
 
   ngOnInit(): void {
+    
+    
     this.getAllUser();
+    /*
     this.getAllJoueur();
     this.getAllVendeur();
     this.getAllAdmin();
+    */
   }
  
 
 
 
-  inscriptionVendeur(vendeur): any{
+  inscriptionVendeur(vendeurs): any{
     //le formulaire s'appelle user, mais creation de vendeur
         //ATTENTION A L'URL
-    this.http.post('http://localhost:8086/vendeur/save', vendeur).subscribe({
+    this.http.post('http://localhost:8086/vendeur/save', vendeurs).subscribe({
       next: (data) => {this.route.navigateByUrl('admin');  },
       error : (err) => { console.log(err); }
 
@@ -34,7 +38,7 @@ export class AdminComponent implements OnInit {
 
   getAllUser(): any{
     this.http.get('http://localhost:8086/user').subscribe({
-      next: (data) => { this.user = data; },
+      next: (data) => { this.users = data; },
       error: (err) => { console.log(err); }
 
     });
@@ -42,30 +46,43 @@ export class AdminComponent implements OnInit {
   }
 
   getAllJoueur(): any{
-    this.http.get('http://localhost:8086/joueur/list').subscribe({
-      next: (data) => { this.joueur = data; },
+    this.http.get('http://localhost:8086/joueur/lis').subscribe({
+      next: (data) => { this.joueurs = data; },
       error: (err) => { console.log(err); }
-
     });
   }
 
     getAllVendeur(): any{
       this.http.get('http://localhost:8086/vendeur/list').subscribe({
-        next: (data) => { this.vendeur = data; },
+        next: (data) => { this.vendeurs = data; },
         error: (err) => { console.log(err); }
-  
       });
-  
     }
 
     getAllAdmin(): any{
       this.http.get('http://localhost:8086/admin/list').subscribe({
-        next: (data) => { this.admin = data; },
+        next: (data) => { this.admins = data; },
         error: (err) => { console.log(err); }
-  
-      });
-  
+        });
     }
+
+  
+
+    rechercheVendeur(recherche): any{
+      
+      this.console(recherche);
+      this.http.post('http://localhost:8086/vendeur/recherche', recherche).subscribe({
+        next: (data) => {  this.recherche = data ; console.log(recherche) ; },
+        error: (err) => { console.log(err); }
+        });
+        this.console("recherche :" + this.recherche);
+        this.console("data :" + this.rechercheV);
+    }
+
+
+    console(entreeConsole): any{
+        console.log(entreeConsole); }
+    
 
   }
 
