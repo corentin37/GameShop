4aa0967d4083f865946e1fe285edac8aea5891fb
AdrienSAVARY <adrien.savary78@laposte.net>
2021-05-17 16:04:01 +0200

    ajout routing panier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,9 @@ import { JeuComponent } from './jeu/jeu.component';
 import { CreerJoueurComponent } from './creer-joueur/creer-joueur.component';
 import { AdminComponent } from './admin/admin.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
-<<<<<<< HEAD
-import { PanierComponent } from './panier/panier.component';
-=======
 import { TacheAdminComponent } from './admin/tache-admin/tache-admin.component';
 import { TacheVendeurComponent } from './admin/tache-vendeur/tache-vendeur.component';
->>>>>>> 44700078ab99512cf9fde6faf9d6b7dd86bd025b
+import { PanierComponent } from './panier/panier.component';
 
 const routes: Routes = [
   {component: JeuComponent, path: 'jeu'},
@@ -18,15 +15,11 @@ const routes: Routes = [
   {component: CreerJoueurComponent, path: 'creerJoueur'},
   {component: AdminComponent, path: 'admin'},
   {component: CatalogueComponent, path: 'catalogue'},
-<<<<<<< HEAD
-  {component: PanierComponent, path: 'panier'},
-  {path:'',redirectTo:'catalogue',pathMatch:'full'}//quand il ya pas de chemin ça redirige directement vers le catalogue
-=======
   {component: TacheAdminComponent, path: 'admin/tacheAdmin'},
   {component: TacheVendeurComponent, path: 'admin/tachevendeur' },
+  {component: PanierComponent, path: 'panier' },
   {path: '', redirectTo: 'catalogue', pathMatch: 'full'}// quand il ya pas de chemin ça redirige directement vers le catalogue
 
->>>>>>> 44700078ab99512cf9fde6faf9d6b7dd86bd025b
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { CreerJoueurComponent } from './creer-joueur/creer-joueur.component';
 import { AdminComponent } from './admin/admin.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
-<<<<<<< HEAD
-import { PanierComponent } from './panier/panier.component';
-=======
 import { TacheAdminComponent } from './admin/tache-admin/tache-admin.component';
 import { TacheVendeurComponent } from './admin/tache-vendeur/tache-vendeur.component';
 import { TacheSalleComponent } from './admin/tache-salle/tache-salle.component';
+import { PanierComponent } from './panier/panier.component';
 
 
->>>>>>> 44700078ab99512cf9fde6faf9d6b7dd86bd025b
 
 
 
@@ -34,15 +31,12 @@ import { TacheSalleComponent } from './admin/tache-salle/tache-salle.component';
     CreerJoueurComponent,
     AdminComponent,
     CatalogueComponent,
-<<<<<<< HEAD
-    PanierComponent
-=======
     TacheAdminComponent,
     TacheVendeurComponent,
     TacheSalleComponent,
-    TacheVendeurComponent
+    TacheVendeurComponent,
+    PanierComponent
 
->>>>>>> 44700078ab99512cf9fde6faf9d6b7dd86bd025b
   ],
   imports: [
     BrowserModule,
