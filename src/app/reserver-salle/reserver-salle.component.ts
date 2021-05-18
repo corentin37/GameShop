f1c00aafdd2faeedbbc99c4bb75ee8e66723ac2a
Damien <damien.lahaussois@gmail.com>
2021-05-18 13:31:07 +0200

    service "salle" et message de réservation

--- src/app/reserver-salle/reserver-salle.component.ts
+++ src/app/reserver-salle/reserver-salle.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SalleService } from '../Services/salle.service';
 
 @Component({
   selector: 'app-reserver-salle',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReserverSalleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private salleService: SalleService) { }
+  salle;
 
   ngOnInit(): void {
+    this.salle=this.salleService.room;
   }
 
 }
