bdb560f7fd8e25cde71e6e78685f84713c8cd71d
AdrienSAVARY <adrien.savary78@laposte.net>
2021-05-18 14:07:37 +0200

    ajout lien back pour panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -13,41 +13,47 @@ export class PanierComponent implements OnInit {
   jeux;
   prixTotalAchat=0;
   prixTotalLocation=0;
-  panier=[{image:"assets/img/monopoly.jpg", note: 4.5, marque: 'Hasbro Gaming', nom : 'Monopoly', prixAchat : 42.99, prixLocation : 15,quantite:2}, 
+  paniertest=[{image:"assets/img/monopoly.jpg", note: 4.5, marque: 'Hasbro Gaming', nom : 'Monopoly', prixAchat : 42.99, prixLocation : 15,quantite:2}, 
   {image:"assets/img/trivial.jpg", note: 4.3, marque: 'Hasbro Gaming', nom: 'Trivial Pursuit', prixAchat : 34.99, prixLocation : 15,quantite:1},
   {image:"assets/img/puissance.jpg", note:4.2, marque: 'Hasbro Gaming', nom : 'Puissance 4', prixAchat : 25.99, prixLocation : 10,quantite:3},
   {image:"assets/img/crocCarotte.jpg", note:4.0, marque: 'Ravensburger', nom : 'Croque-Carotte', prixAchat : 25.99, prixLocation : 8,quantite:1},
   {image:"assets/img/risk.jpg", note:4.6, marque: 'Hasbro Gaming', nom : 'Risk', prixAchat : 38.99, prixLocation : 15,quantite:1},
   {image:"assets/img/elefun.jpg", note:3.8, marque: 'Hasbro Gaming', nom : 'Elefun', prixAchat : 27.99, prixLocation : 8,quantite:1}
   ];
-  panier2; 
+  panier;
   alljeux: Object;
+  iduser=5;
 
   constructor(private http: HttpClient,private route: Router) { }
 
   ngOnInit(): void {
-    this.getCatalogue();
+    this.getOnePanierByUser(this.iduser);
+    //this.getAllPaniers();
     this.getPrixTotaux();
   }
   
-  getOneJeu(idjeu){
-    this.http.get('http://localhost:8086/jeu/4').subscribe({
-      next: (data) => {this.jeu = data;},
+  getOnePanierByUser(iduser){
+    this.http.get('http://localhost:8086/panier/user/'+iduser).subscribe({
+      next: (data) => {this.panier = data;},
       error: (err) => {console.log(err);}
     });
   }
 
-  getCatalogue() : any{
+  getAllPaniers() : any{
     this.http.get('http://localhost:8086/paniers').subscribe({
-      next: (data)=> {this.panier2 = data; },
+      next: (data)=> {this.panier = data; },
       error: (err)=> {console.log(err);}
       });
     }
   
+  getPanierAchat():any{
+
+  }
+  
   getPrixTotaux():any{
     this.prixTotalAchat=0;
     this.prixTotalLocation=0;
-        for (let j of this.panier) {
+    for (let j of this.panier) {
       this.prixTotalAchat+=j.prixAchat*j.quantite;
       this.prixTotalLocation+=j.prixLocation*j.quantite;
     }
