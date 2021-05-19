535ecf013beee9339b758a99328d9140e1455d98
corentin <corentin.brochon@consultants-solutec.fr>
2021-05-19 14:55:44 +0200

    modif component question

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -12,10 +12,10 @@ export class ForumComponent implements OnInit {
   constructor(private http: HttpClient, private route: Router) { }
  sujets;
  message;
- idforum=localStorage.getItem("id");
+ 
+ idforum=localStorage.getItem("idforum");
   ngOnInit(): void {
     this.getSujet();
-    this.goToMessageByForumId(this.idforum);
   }
   
 
@@ -25,12 +25,24 @@ export class ForumComponent implements OnInit {
       error: (err)=> {console.log(err);}
     })
   }
-  goToMessageByForumId(idforum){
+  /*goToMessageByForumId(idforum){
     this.http.get('http://localhost:8086/messages/forum/'+idforum).subscribe({
-      next: (data) => {this.message = data;console.log("panier : ",this.message)},
+      next: (data) => {this.message = data; console.log(data); 
+        a.forEach(element => {
+          
+        });
+        localStorage.setItem("idforum",getForumId(message))},
       error: (err) => {console.log(err);}
     });
+  }*/
+  getForumId(forum): any{
+    this.http.get ('http://localhost:8086/forum/').subscribe({
+      next: (data)=> {this.sujets = data;console.log(data) },
+      error: (err)=> {console.log(err);}
+    })
   }
+
+
   goToQuestions(): any {
     this.route.navigateByUrl('questionLivraison');
   }
