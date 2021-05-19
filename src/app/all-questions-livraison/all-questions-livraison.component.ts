c5a5357f97eebc4173e8c7a7deff8755a79fddab
corentin <corentin.brochon@consultants-solutec.fr>
2021-05-20 00:03:45 +0200

    modif all questions livraisons et forum pour affichage des nouveaux messages par sujets spécifiques

--- src/app/all-questions-livraison/all-questions-livraison.component.ts
+++ src/app/all-questions-livraison/all-questions-livraison.component.ts
@@ -12,12 +12,14 @@ export class AllQuestionsLivraisonComponent implements OnInit {
 
   constructor(private http: HttpClient, private route: Router,  private formService: ForumService) { }
 message;
-
+login;
 info;
+msg;
+forum;
   ngOnInit(): void {
     this.getAllMessage();
     this.getElementBySubject();
-    
+    //this.login=localStorage.getItem("id");
   }
 
   getElementBySubject(): any {
@@ -28,14 +30,38 @@ info;
   }
   
 
-  newMessage(msg):any{//note ici est la valeur du formulaire
+  /*newMessage(msg):any{//note ici est la valeur du formulaire
     const expediteur = {id: 1}; //en dur, en temps normal récupérer id de l'user qui s'est connecté
     msg.expediteur = expediteur;
     this.http.post('http://localhost:8086/message', msg).subscribe ({//note est le request body
     next: (data)=>{this.route.navigateByUrl('questionLivraison');this.ngOnInit()},
     error: (err)=>{console.log(err);}
     });
-  }
+  }*/
+  newMessage(msg): any {
+    console.log("Message posté! Rafraîchir la page");
+    //const user ={id :localStorage.getItem("id")};
+    const expediteur = {id: 1};
+   // msg.user=user;
+    msg.expediteur=expediteur;
+    //msg.forum=this.forum.sujet;
+    //if(!this.login){
+    //  return confirm('Veuillez vous connecter');
+    //}
+    //else {
+      
+      this.http.post('http://localhost:8086/message/'+ this.formService.subjectForum.id, msg).subscribe({
+        next: (data) => {console.log(data); this.ngOnInit(); return confirm('Votre question a bien été prise en compte, un collaborateur y répondra très bientôt !'); },
+        error: (err) => {console.log(err); }
+      });
+    }
+  //}
+
+  
+
+
+
+
 
 
   
