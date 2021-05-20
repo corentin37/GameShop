0a7e989b4398f7a61f29365e78fb6ba2f06fcfdc
possart <alexis.cadoux@gmail.com>
2021-05-20 13:52:30 +0200

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { DeconnexionComponent } from './deconnexion/deconnexion.component';
 import { AjouterSalleComponent } from './ajouter-salle/ajouter-salle.component';
 import { ProfilJoueurComponent } from './profil-joueur/profil-joueur.component';
 import { ModifierCotisationComponent } from './modifier-cotisation/modifier-cotisation.component';
+import { MaMessagerieComponent } from './ma-messagerie/ma-messagerie.component';
 
 
 
@@ -53,7 +54,7 @@ import { ModifierCotisationComponent } from './modifier-cotisation/modifier-coti
     ForumComponent,
     AllQuestionsLivraisonComponent,
     ProfilJoueurComponent,
-    MaMessagerieComponent
+    MaMessagerieComponent,
     ModifierCotisationComponent
 
   ],
