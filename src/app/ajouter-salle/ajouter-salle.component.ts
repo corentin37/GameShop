5f80210b0f61680badea72d83ff76688afeafda6
Damien <damien.lahaussois@gmail.com>
2021-05-20 09:38:52 +0200

    Ajouter une salle fonctionnel

--- src/app/ajouter-salle/ajouter-salle.component.ts
+++ src/app/ajouter-salle/ajouter-salle.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { error } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-ajouter-salle',
@@ -7,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjouterSalleComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  salleAjoutee;
+  message;
   ngOnInit(): void {
   }
 
   ajouterSalle(salle){
-    
+    this.http.post('http://localhost:8086/salle/save', salle).subscribe({
+    next: (data) => {console.log(data); 
+    this.salleAjoutee=data; },
+    error: (err) => {console.log(err);
+    }
+    });
   }
 
+  messageAjout(): any{
+    if(this.salleAjoutee!=null){
+      this.message="Salle ajoutée."
+    }else{
+      this.message=""
+    }
+    return this.message;
+  }
 }
